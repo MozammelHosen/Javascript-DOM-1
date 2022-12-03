f98e22011b844da2914a7e5d44847f22344e7411
MozammelHosen <mdmozammelhosen15701@gmail.com>
2022-12-04 01:17:49 +0600

    first commit

---
+++ Main.js
@@ -0,0 +1,11 @@
+const h3tag = document.getElementsByTagName("h3");
+// for off loop
+for (const h3 of h3tag) {
+  console.log(h3.innerText);
+}
+const ptag = document.getElementsByTagName("p");
+for (const p of ptag) {
+  console.log(p.innerText);
+}
+const idtag = document.getElementById("MyBlogs2");
+idtag.style.textAlign="center"
